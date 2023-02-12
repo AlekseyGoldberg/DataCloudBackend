cee34b66d221a2ab3356993a91dc80da43a2a2c4
alekseyZhizhin <zhizhin309@yandex.ru>
2023-02-12 13:21:43 +0300

    Add exceptions and text message

---
+++ src/main/java/com/example/diplom/text/Message.java
@@ -0,0 +1,8 @@
+package com.example.diplom.text;
+
+public class Message {
+    public static String INTERNAL_SERVICE_ERROR = "Internal server error";
+    public static String INVALID_TOKEN = "invalid token";
+
+    public static String FILE_THIS_NAME_EXIST = "file with this name exists";
+}
